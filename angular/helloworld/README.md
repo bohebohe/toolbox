69463a4c8f121e7d946c7b123c56f77fa1422b42
bohebohe <ashibuya0128@gmail.com>
2016-04-29 18:34:47 +0900

    to understand angular and angular-cookie for myself

---
+++ angular/helloworld/README.md
@@ -0,0 +1,17 @@
+# Angular Helloworld Cookie sample
+
+to understand angular and angular-cookie for myself
+
+## use
+
+- express
+- angular
+- angular-cookies
+
+## run
+
+- bower install
+- npm install
+- npm start
+
+http://localhost:3000
--- angular/helloworld/js/MyAppCtrl.js
+++ angular/helloworld/js/MyAppCtrl.js
@@ -1,15 +1,4 @@
 'use strict';
 angular.module('MainController', []).
   controller('MainCtrl', [ "$scope",function($scope) {
-    $scope.name = 'hokaccha';
-
-    // $scope.onClickBtn = function(){
-    //   $scope.name = $scope.name.split('').reverse().join('');
-
-
-    // $scope.people=[
-    //     {firstName:'AAAA', lastName:'aaaaaa'},
-    //     {firstName:'BBBB', lastName:'bbbbbb'},
-    //     {firstName:'CCCC', lastName:'cccccc'},
-    // ];
 }]);
